86efd50dfd569be0e3a0945e25913927a6b8310c
Stephen Campbell <stephenjc77@gmail.com>
2026-07-22 10:52:35 -0400

    Configure custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://stephenjc77.github.io",
-  base: "/campbell-intelligence",
+  site: "https://campbellintelligence.com",
 });
